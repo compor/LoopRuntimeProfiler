afccc002763751fdd5df588e341d2940e2b9889e
Chris Vassiladiotis <cvassiladiotis@gmail.com>
2017-07-20 15:42:19 +0100

    add dummy version for runtime

---
+++ runtime/lrp_rt.c
@@ -0,0 +1,10 @@
+
+static void lrp_report(void) { return; }
+
+static void lrp_program_start(void) { return; }
+
+static void lrp_program_stop(void) { return; }
+
+static void lrp_loop_start(void) { return; }
+
+static void lrp_loop_stop(void) { return; }
