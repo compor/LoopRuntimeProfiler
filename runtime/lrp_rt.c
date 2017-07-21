a13097f47d58c997aaafcdbea090bd49466dca05
Chris Vassiladiotis <cvassiladiotis@gmail.com>
2017-07-21 15:40:04 +0100

    implement program timing functions

--- runtime/lrp_rt.c
+++ runtime/lrp_rt.c
@@ -1,9 +1,54 @@
+//
+//
+//
 
-static void lrp_report(void) { return; }
+#include <time.h>
+// using clock
+// using CLOCKS_PER_SEC
 
-static void lrp_program_start(void) { return; }
+#include <stdlib.h>
+// using atexit
+// using abort
 
-static void lrp_program_stop(void) { return; }
+#include <stdio.h>
+// using fprintf
+
+static clock_t lrp_program_start_timestamp;
+static clock_t lrp_program_stop_timestamp;
+
+static void lrp_report(void) {
+  double duration = 1000.0 *
+                    (lrp_program_stop_timestamp - lrp_program_start_timestamp) /
+                    CLOCKS_PER_SEC;
+
+  fprintf(stderr, "lrp runtime (ms): %f\n", duration);
+
+  return;
+}
+
+static void lrp_program_stop(void) {
+  fprintf(stderr, "lrp runtime stop!\n");
+  lrp_program_stop_timestamp = clock();
+
+  lrp_report();
+
+  return;
+}
+
+static void lrp_program_start(void) {
+  int rc = atexit(lrp_program_stop);
+
+  if (!rc) {
+    fprintf(stderr, "could not set program exit handler!\n");
+
+    abort();
+  }
+
+  fprintf(stderr, "lrp runtime start!\n");
+  lrp_program_start_timestamp = clock();
+
+  return;
+}
 
 static void lrp_loop_start(void) { return; }
 
